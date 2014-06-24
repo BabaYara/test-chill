d974512b1e1ab180d9e0ae9947764cf76163d55d
Derick Huth <derickhuth@gmail.com>
2014-06-23 21:05:10 -0600

    Update mm.c

--- test-cases/chill/mm.c
+++ test-cases/chill/mm.c
@@ -1,4 +1,11 @@
-
+// test mm(A,B,C,AMBN,AN,BM)
+// in int AMBN range(4,32)
+// in int AN range(4,32)
+// in int BM range(4,32)
+// in float** A matrix(AN,AMBN,range(-5,5))
+// in float** B matrix(AMBN,BM,range(-5,5))
+// out float** C matrix(AN,BM)
+// end test
 
 void mm(float **A, float **B, float **C, int ambn, int an, int bm) {
   int i, j, n;
